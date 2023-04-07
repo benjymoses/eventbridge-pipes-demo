27587120989573872d7b61b8e39dd4376a4c1dff
Ben Moses <benmoses@amazon.co.uk>
2023-04-07 16:21:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# EventBridge Pipes Demo
+
+This is a personal learning project by Ben Moses. The objective is to learn AWS CDK by doing, and to experiment with [EventBridge Pipes](https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-pipes.html).
+
+> ⚠️ Deploying any resources to an AWS account that you own may incur charges and you are responsible for managing this. See [here](https://aws.amazon.com/blogs/aws-cloud-financial-management/getting-started-with-aws-budgets/) for instructions on how to manage costs.
+
+> This code come with zero warranty and you are responsible for activity in your AWS account.
+
+If you have any feedback, or ideas for me to improve the TypeScript quality in this project, please Tweet me @benmosesUK
+
+## How to use this demo
+This demo uses EventBridge Pipes to connect a DynamoDB stream to an EventBridge bus to emit domain events. While message are travelling down the pipe, they are filtered and enriched. 
+
+The filtering uses the same pattern types as EventBridge Rules. In the context of this demo they filter out all messages apart from `INSERT` messages in to the DynamoDB table that have the required fields present.
+
+Enrichment is provided by a Step Functions Express workflow that performs two actions. First, it reformats the message in to the shape for a domain event. Lastly, it mocks a call to a fraud checking service by using a `Pass` state and the `States.MathRandom()` [intrinsic function](https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-intrinsic-functions.html).
+
+> Insert architecture diagram
+
+
+### Deployment instructions
+  
+> 💡 **Assumption**: you must have `npm` and `cdk` installed to use this demo as well as `AWS CLI` with a configured profile. [NPM](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm/) | [AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html) | [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
+  
+1. In the root of the project is a `config.ts` file. Open this and update the value of `notificationEmail` from *'nowhere@example.com'* to your email address. (this is the e-mail address SNS will use to send "fraud" order messages to)
+2. In the root of the project, in your terminal run `npm install` to install all project dependencies.
+3. *(Optional)* if you have never used AWS CDK with your target AWS account then you need to run `cdk bootstrap` to prepare your account for use with AWS CDK.
+4. Once you're ready run `cdk deploy`. 
+
+The stack is now deployed and ready to use.
+
+### Using the demo
+
+### Deleting the stack
+When you are ready to delete the stack simply run `cdk destroy` and confirm that you want to delete the stack.
+
+> ⚠️ If you have modified any resources in the console then deletion may fail. If it does, open the [CloudFormation Console](https://console.aws.amazon.com/cloudformation/), click in to the stack and look at the errors in the *Events* tab to troubleshoot.
+
+---
+
+## Outstanding ideas
+- Build a CloudWatch dashboard that shows the end-to-end metrics from entry of an item in to the DynamoDB table, the DynamoDB stream, EventBridge Pipe, Step Functions enrichment workflow, and the EventBridge bus.
+- Build a test suite to extend the scaffolded jest framework.
+- When this demo was created (April 2023), there was no Level 2 CDK Construct for EventBridge Pipes. There is an [open Github issue](https://github.com/aws/aws-cdk-rfcs/issues/473) for a community contribution of an L2 Construct. Once an L2 construct exists, this demo will be updated to make use of it.
+
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
---
+++ lib/eb-pipes-demo-stack.ts
@@ -0,0 +1,163 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+import { Table, AttributeType, BillingMode, StreamViewType} from 'aws-cdk-lib/aws-dynamodb'
+import { EventBus, Rule, Match } from 'aws-cdk-lib/aws-events'
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import { CfnPipe } from 'aws-cdk-lib/aws-pipes'
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { Subscription, SubscriptionProtocol, Topic } from 'aws-cdk-lib/aws-sns'
+import { PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+
+import { config } from '../config';
+
+export class EbPipesDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB 'Orders' table'
+    const ddbTable = new Table(this, 'OrdersTable', {
+      partitionKey: { name: 'PK', type: AttributeType.STRING},
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      stream: StreamViewType.NEW_IMAGE,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+
+    // Custom EventBridge Bus
+    const eventBus = new EventBus(this, 'OrdersBus', {
+      //eventBusName: 'OrdersBus'
+    })
+
+    // EventBridge rule to catch-all to CloudWatch Logs (enable for demo purposes)
+    const catchAllLogRule = new Rule(this, 'CatchAllLogRule', {
+      description: 'Used to capture all events on the custom EventBridge bus for demo purposes',
+      ruleName: 'CatchAllLogRule',
+      eventBus: eventBus,
+      eventPattern: {
+        source: Match.prefix(''),
+      },
+      targets: [new targets.CloudWatchLogGroup(new LogGroup(this, 'CatchAllLogGroup', {
+        logGroupName: '/aws/events/catchall-pipes-demo',
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      }))],
+    });
+
+    // Policy to allow EventBridge to write to CloudWatch Logs
+    new PolicyDocument({
+      statements: [
+        new PolicyStatement({
+          resources: [
+            `arn:aws:logs:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:log-group:/aws/events/*:*"`,
+          ],
+          actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+          principals: [new ServicePrincipal("events.amazonaws.com"), new ServicePrincipal("delivery.logs.amazonaws.com")],
+        }),
+      ],
+    });
+
+    // SNS topic and subscription for 'fraud' events to go to
+    const snsTopic = new Topic(this, 'FraudTopic')
+    new Subscription(this, 'FraudTeamSubscription', {
+      protocol: SubscriptionProtocol.EMAIL,
+      topic: snsTopic,
+      endpoint: config.notificationEmail
+    })
+
+    // Rule on EventBridge for high fraud scores
+    const fraudRule = new Rule(this, 'HighFraudRule', {
+      description: 'All orders with a Fraud Score higher than 80 go to fraud team email',
+      eventBus: eventBus,
+      eventPattern: { 
+        source: Match.prefix("fraudcheck.orderpipe"),
+        detail: {
+          fraudChecks: {
+            fraudScore: Match.greaterThan(80)
+          }
+        }
+      },
+      targets: [new targets.SnsTopic(snsTopic)],
+      //ruleName: 'fraud-rule',
+    })
+
+    // Step Functions workflow to enrich contents of the pipe
+    const enricherWorkflow = new stepfunctions.StateMachine(this, 'EnricherWorkflowForFraud', {
+      //stateMachineName: 'enricherWorkflow',
+      stateMachineType: stepfunctions.StateMachineType.EXPRESS,
+      tracingEnabled: true,
+      logs: {
+        destination: new LogGroup(this, 'OrderEnrichmentLogs', {
+          retention: RetentionDays.THREE_DAYS,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        }),
+        level: stepfunctions.LogLevel.ALL,
+        includeExecutionData: true
+      },
+      definition: stepfunctions.Chain.start(
+        new stepfunctions.Map(this, 'Map over events in batch', {
+          maxConcurrency: 5,
+          //itemsPath: stepfunctions.JsonPath.stringAt('$')
+        })
+        .iterator(
+          new stepfunctions.Pass(this, 'Deserialise DynamoDB', {
+            parameters: {
+              eventData: {
+                eventID: stepfunctions.JsonPath.stringAt('$.eventID'),
+                eventName: stepfunctions.JsonPath.stringAt('$.eventName'),
+                approxDateTime: stepfunctions.JsonPath.numberAt('$.dynamodb.ApproximateCreationDateTime')
+              },
+              orderData: {
+                orderNumber: stepfunctions.JsonPath.stringAt('$.dynamodb.NewImage.PK.S'),
+                customerName: stepfunctions.JsonPath.stringAt('$.dynamodb.NewImage.name.S'),
+                orderValue: stepfunctions.JsonPath.stringToJson(stepfunctions.JsonPath.stringAt('$.dynamodb.NewImage.value.N'))
+              }
+            } // Parameter creates new JSON object from contents of original input payload
+          })
+          .next(new stepfunctions.Pass(this, 'Fraud Check', {
+            resultPath: '$.fraudChecks',
+            parameters: {fraudScore: stepfunctions.JsonPath.mathRandom(1,100)}
+          })) // This Pass state simulates calling out to an API or service that provides a Fraud Score back based on the orderData it was passed as input
+        )
+      )
+    })
+
+    // IAM Role for EventBridge Pipes
+    const pipesRole = new Role(this, 'pipesRole', {
+      assumedBy: new ServicePrincipal('pipes.amazonaws.com')
+    })
+    ddbTable.grantStreamRead(pipesRole)
+    eventBus.grantPutEventsTo(pipesRole)
+    enricherWorkflow.grantStartSyncExecution(pipesRole)
+  
+    // Filter Criteria for EventBridge Pipes source filtering
+    const filterInsertsWithFields: CfnPipe.FilterCriteriaProperty = {
+      filters: [{
+        pattern: "{\"eventName\":[\"INSERT\"],\"dynamodb\":{\"NewImage\":{\"PK\":{\"S\":[{\"exists\":true}]},\"name\":{\"S\":[{\"exists\":true}]},\"value\":{\"N\":[{\"exists\":true}]}}}}",
+      }],
+    }; 
+
+  // EventBridge Pipe to connect DynamoDB stream to EventBridge bus with in-line fraud checking enrichment by Step Functions.
+    if (typeof ddbTable.tableStreamArn === "string") { // CfnPipe.source is a required Param and TS is unable to assert it won't be undefined
+    const orderFraudPipe = new CfnPipe(this, 'OrderFraudPipe', {
+      roleArn: pipesRole.roleArn,
+      source: ddbTable.tableStreamArn,
+      target: eventBus.eventBusArn,
+      enrichment: enricherWorkflow.stateMachineArn,
+      sourceParameters: {
+        dynamoDbStreamParameters: {
+          startingPosition: "LATEST",
+          batchSize: 5,
+          onPartialBatchItemFailure: "AUTOMATIC_BISECT",
+        },
+        filterCriteria: filterInsertsWithFields,
+      },
+      targetParameters: { 
+        eventBridgeEventBusParameters: {
+          detailType: 'fraudcheck.rating',
+          source: 'fraudcheck.orderpipe'
+        }
+      }      
+    })
+    }
+  }
+}
